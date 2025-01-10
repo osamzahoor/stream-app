233ba128f8e511359a42352826899e755a0e282e
osamzahoor <osamzahoor@gmail.com>
2025-01-10 09:47:48 -0800

    remove extra linkfrom header and change azure blob credientials

--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -21,9 +21,6 @@ const user = {
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
   { name: "Feed", href: "/list", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
 ];
 const userNavigation = [
   { name: "Your Profile", href: "/profile" },
